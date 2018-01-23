3f0173d46a24480a3a05f579072f42292002c4b3
gallimaufrystew <33710058+gallimaufrystew@users.noreply.github.com>
2018-01-23 18:09:10 +0800

    Update ssl_client_libssl.c

--- ssl_client_libssl.c
+++ ssl_client_libssl.c
@@ -1,143 +1,209 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-/* #include <sys/un.h> */
+#include <arpa/inet.h>
+#include <unistd.h>
 #include <unistd.h>
 #include <string.h>
 
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 
-#define SSL_CLIENT_RSA_CERT	"/home/labia/certs/client.crt"
-#define SSL_CLIENT_RSA_KEY	"/home/labia/certs/client.key"
-#define SSL_CLIENT_RSA_CA_CERT	"/home/labia/certs/ca.crt"
-#define SSL_CLIENT_RSA_CA_PATH	"/home/labia/certs/"
+#define TRUE   0
+#define FALSE  1
 
-#define OFF	0
-#define ON	1
+#define CUSTOM_EXT_TYPE_1000 10000
 
-int main(void)
+const char cust_str[] = "sign_algo=sha2";
+
+X509 *load_cert(const char *file);
+SSL_CTX *create_ssl_ctx(const char *sign_algo);
+static int add_cust_ext_callback(SSL *s, unsigned int ext_type,
+                                 const unsigned char **out,
+                                 size_t *outlen, int *al, void *arg);
+int main()
 {
-	int verify_peer = ON;
-	SSL_METHOD *client_meth;
-	SSL_CTX *ssl_client_ctx;
-	int clientsocketfd;
-	/* struct sockaddr_un serveraddr; */
-	struct sockaddr_in serveraddr;
-	int handshakestatus;
-	SSL *clientssl;
-	char buffer[1024] = "Client Hello World";
-	int ret;
+    int verify_peer = TRUE;
+    SSL_CTX *ssl_ctx;
+    int fd;
+    struct sockaddr_in addr;
 
-	SSL_library_init();
-	SSL_load_error_strings();
-	client_meth = SSLv3_client_method();
-	ssl_client_ctx = SSL_CTX_new(client_meth);
-	
-	if(!ssl_client_ctx)
-	{
-		ERR_print_errors_fp(stderr);
-		return -1;
-	}
-
-	if(verify_peer)
-	{	
-	
-		if(SSL_CTX_use_certificate_file(ssl_client_ctx, SSL_CLIENT_RSA_CERT, SSL_FILETYPE_PEM) <= 0)	
-		{
-			ERR_print_errors_fp(stderr);
-			return -1;		
-		}
-
-	
-		if(SSL_CTX_use_PrivateKey_file(ssl_client_ctx, SSL_CLIENT_RSA_KEY, SSL_FILETYPE_PEM) <= 0)	
-		{
-			ERR_print_errors_fp(stderr);
-			return -1;		
-		}
-	
-		if(SSL_CTX_check_private_key(ssl_client_ctx) != 1)
-		{
-			printf("Private and certificate is not matching\n");
-			return -1;
-		}	
-
-		//See function man pages for instructions on generating CERT files
-		if(!SSL_CTX_load_verify_locations(ssl_client_ctx, SSL_CLIENT_RSA_CA_CERT, NULL))
-		{
-			ERR_print_errors_fp(stderr);
-			return -1;		
-		}
-		SSL_CTX_set_verify(ssl_client_ctx, SSL_VERIFY_PEER, NULL);
-		SSL_CTX_set_verify_depth(ssl_client_ctx, 1);
-	}
-	
-	if((clientsocketfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		printf("Error on socket creation\n");
-		return -1;
-	}
+    SSL *ssl;
+    char buffer[1024] = "Client Hello World";
+    int ret;
     
-    memset(&serveraddr, 0, sizeof(struct sockaddr_in));
-    serveraddr.sin_family = AF_INET;
-    serveraddr.sin_port = htons(443);
-    serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-/*
-	if((clientsocketfd = socket(AF_UNIX, SOCK_STREAM, 0)) < 0)
-	{
-		printf("Error on socket creation\n");
-		return -1;
-	}
-	memset(&serveraddr, 0, sizeof(struct sockaddr_un));
-	serveraddr.sun_family = AF_UNIX;
-	serveraddr.sun_path[0] = 0;
-	strncpy(&(serveraddr.sun_path[1]), SSL_SERVER_ADDR, strlen(SSL_SERVER_ADDR) + 1);
-*/
-	
-	connect(clientsocketfd, (struct sockaddr *)&serveraddr, sizeof(struct sockaddr_in));
-		
-	clientssl = SSL_new(ssl_client_ctx);
-	if(!clientssl)
-	{
-		printf("Error SSL_new\n");
-		return -1;
-	}
-	SSL_set_fd(clientssl, clientsocketfd);
-		
-	if((ret = SSL_connect(clientssl)) != 1)
-	{
-		printf("Handshake Error %d\n", SSL_get_error(clientssl, ret));
-		return -1;
-	}
-		
-	if(verify_peer)
-	{
-		X509 *ssl_client_cert = NULL;
-
-		ssl_client_cert = SSL_get_peer_certificate(clientssl);
-			
-		if(ssl_client_cert)
-		{
-			long verifyresult;
-
-			verifyresult = SSL_get_verify_result(clientssl);
-			if(verifyresult == X509_V_OK)
-				printf("Certificate Verify Success\n"); 
-			else
-				printf("Certificate Verify Failed\n"); 
-			X509_free(ssl_client_cert);				
-		}
-		else
-			printf("There is no client certificate\n");
-	}
-	SSL_write(clientssl, buffer, strlen(buffer) + 1);
-	SSL_read(clientssl, buffer, sizeof(buffer));
-	printf("SSL server send %s\n", buffer);
-	SSL_shutdown(clientssl);
-	close(clientsocketfd);
-	SSL_free(clientssl);
-	SSL_CTX_free(ssl_client_ctx);
     
-	return 0;	
+    SSL_library_init();
+    SSL_load_error_strings();
+
+    OpenSSL_add_all_algorithms();
+
+    ssl_ctx = create_ssl_ctx("sha2");
+
+    if (!ssl_ctx) {
+        ERR_print_errors_fp(stderr);
+        return -1;
+    }
+
+    SSL_CTX_add_server_custom_ext(ssl_ctx, CUSTOM_EXT_TYPE_1000,
+                                  add_cust_ext_callback,
+                                  NULL, NULL,NULL, NULL);
+
+    if ((fd = socket(/* AF_UNIX */AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Error on socket creation\n");
+        return -1;
+    }
+
+    memset(&addr, 0, sizeof(struct sockaddr_in));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(4433);
+    addr.sin_addr.s_addr = INADDR_ANY;//inet_addr("10.123.162.58");/*  */
+
+    connect(fd, (struct sockaddr *)&addr, sizeof(struct sockaddr_in));
+
+    ssl = SSL_new(ssl_ctx);
+    if (!ssl) {
+        printf("Error SSL_new\n");
+        return -1;
+    }
+    SSL_set_fd(ssl, fd);
+
+    if ((ret = SSL_connect(ssl)) != 1) {
+        ERR_print_errors_fp(stderr);
+        printf("Handshake Error %d\n", SSL_get_error(ssl, ret));
+        return -1;
+    }
+
+    if (verify_peer) {
+        X509 *cert = SSL_get_peer_certificate(ssl);
+        if (cert) {
+            long ret = SSL_get_verify_result(ssl);
+            if (ret != X509_V_OK) {
+                printf("Certificate Verify Failed\n");
+                goto fail;
+            } else {
+                printf("Verify OK\n");
+            }
+            X509_free(cert);
+        } else {
+            printf("There is no client certificate\n");
+        }
+    }
+    
+    SSL_write(ssl, buffer, strlen(buffer) + 1);
+    SSL_read(ssl, buffer, sizeof(buffer));
+    printf("SSL server send %s\n", buffer);
+    
+fail:    
+    SSL_shutdown(ssl);
+    close(fd);
+    SSL_free(ssl);
+    SSL_CTX_free(ssl_ctx);
+
+    return 0;
+}
+
+static int add_cust_ext_callback(SSL *s, unsigned int ext_type,
+                                 const unsigned char **out,
+                                 size_t *outlen, int *al, void *arg)
+{
+    
+    printf("-----add cust ext-----\n");
+    
+    *out = (const unsigned char *)cust_str;
+    *outlen = strlen(cust_str);
+
+    return 1;
+}
+
+X509 *load_cert(const char *file)
+{
+    X509   *x = NULL;
+    BIO    *err = NULL, *cert = NULL;
+
+    cert = BIO_new(BIO_s_file());
+    if (cert == NULL) {
+        ERR_print_errors(err);
+        goto end;
+    }
+
+    if (BIO_read_filename(cert, file) <= 0) {
+        BIO_printf(err, "Error opening %s\n", file);
+        ERR_print_errors(err);
+        goto end;
+    }
+
+    x = PEM_read_bio_X509_AUX(cert, NULL, NULL, NULL);
+
+end:
+    if (x == NULL) {
+        BIO_printf(err, "unable to load certificate\n");
+        ERR_print_errors(err);
+    }
+    if (cert != NULL) {
+        BIO_free(cert);
+    }
+    return (x);
+}
+
+SSL_CTX *create_ssl_ctx(const char *sign_algo)
+{
+    SSL_CTX *ssl_ctx = NULL;
+    ssl_ctx = SSL_CTX_new(SSLv23_client_method());
+    if (!ssl_ctx) {
+        ERR_print_errors_fp(stderr);
+        return NULL;
+    }
+
+    SSL_CTX_add_client_custom_ext(ssl_ctx, CUSTOM_EXT_TYPE_1000,
+                                  add_cust_ext_callback,
+                                  NULL, NULL,
+                                  NULL, NULL);
+
+    char file_name[512] = {0};
+    sprintf(file_name, "client_%s.crt", sign_algo);
+
+#if (1)
+    //SSL_CTX_use_certificate_file SSL_FILETYPE_PEM
+    if (SSL_CTX_use_certificate_file(ssl_ctx, file_name,SSL_FILETYPE_PEM) <= 0) {
+        //printf("SSL_CTX_use_certificate_file() fail");
+        ERR_print_errors_fp(stderr);
+        return NULL;
+    }
+#else
+    X509 *x509 = load_cert(file_name);
+
+    if (SSL_CTX_use_certificate(ssl_ctx, x509) <= 0) {
+        //printf("SSL_CTX_use_certificate_file() fail");
+        ERR_print_errors_fp(stderr);
+        return NULL;
+    }
+    X509_free(x509);
+#endif
+
+    sprintf(file_name, "client_%s.key", sign_algo);
+    if (SSL_CTX_use_PrivateKey_file(ssl_ctx, file_name, SSL_FILETYPE_PEM) <= 0) {
+        //printf("SSL_CTX_use_PrivateKey_file() fail");
+        ERR_print_errors_fp(stderr);
+        return NULL;
+    }
+
+    if (SSL_CTX_check_private_key(ssl_ctx) != 1) {
+        //printf("Private and certificate is not matching\n");
+        ERR_print_errors_fp(stderr);
+        return NULL;
+    }
+
+    // we can string certs together to form a cert-chain
+    sprintf(file_name, "ca_%s.crt", sign_algo);
+    if (!SSL_CTX_load_verify_locations(ssl_ctx, file_name, NULL)) {
+        ERR_print_errors_fp(stderr);
+        return NULL;
+    }
+    SSL_CTX_set_verify(ssl_ctx, SSL_VERIFY_PEER, NULL);
+    //SSL_CTX_set_verify_depth(ssl_ctx, 1);
+
+    //SSL_CTX_set_tlsext_servername_callback(ssl_ctx, svr_name_callback);
+
+    return ssl_ctx;
 }
